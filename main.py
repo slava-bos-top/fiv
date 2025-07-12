7c4af4701346a5bc2cab3fa4f056ec4a457f6b10
slava-bos-top <105057483+slava-bos-top@users.noreply.github.com>
2025-07-12 20:03:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -198,21 +198,11 @@ async def register_city(message: Message, state: FSMContext):
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive",
     ]
-    cred_json_str = os.getenv("GOOGLE_CREDENTIALS")
+    cred_json_str = Config.GOOGLE_CREDENTIALS
 
     cred_dict = json.loads(cred_json_str)
 
     creds = ServiceAccountCredentials.from_json_keyfile_dict(cred_dict, scope)
-    client = gspread.authorize(creds)
-    spreadsheet = client.open_by_url(
-        "https://docs.google.com/spreadsheets/d/17lcrlxUhcervwQTOctLZkdvBVpAwyuWu7DQQ3d_oVSQ/edit?usp=sharing"
-    )
-
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive",
-    ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("cred.json", scope)
     client = gspread.authorize(creds)
 
     sheet = client.open_by_url(
