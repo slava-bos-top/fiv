efa764cd975ddf5892ddecd3d8335e7b413d76fe
slava-bos-top <105057483+slava-bos-top@users.noreply.github.com>
2025-07-16 20:10:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,114 @@ class Register(StatesGroup):
     password = State()
     number = State()
     yourNumber = State()
+    num = State()
+    first_name = State()
+    last_name = State()
+    numbers = State()
 
+
+@router.message(CommandStart(deep_link=True))
+async def start_handler(message: Message, state: FSMContext):
+    chat_id = message.chat.id
+    user = message.from_user
+    await state.update_data(first_name=[user.first_name])
+    await state.update_data(last_name=[user.last_name])
+    user_id = message.from_user.id
+    text = message.text
+
+    # 🔍 Витягуємо аргумент після /start
+    if message.text.startswith("/start "):
+        param = message.text.split(" ", 1)[1]  # ← буде "phone_380501234567"
+
+        if param.startswith("phone_"):
+            await state.set_state(Register.numbers)
+            phone = param.replace("phone_", "")
+            await message.answer(
+                f"👋 Вітаю! Надішліть посилання з номером, для підтвердження номера.",
+                reply_markup=kb.get_number,
+            )
+            print(f"Chat ID: {chat_id}, Phone: {phone}")
+            await state.update_data(num=[phone])
+        else:
+            await message.answer("❌ Невірний формат параметра.")
+    else:
+        await message.answer("👋 Вітаю! Надішліть посилання з номером.")
+
+
+@router.message(StateFilter(Register.numbers), F.contact)
+async def register_city(message: Message, state: FSMContext):
+    await message.answer("Дані зберігаються...", reply_markup=ReplyKeyboardRemove())
+    await state.update_data(number=message.contact)
+    data = await state.get_data()
+    number = list(data["numbers"])[0][1]
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    # creds = ServiceAccountCredentials.from_json_keyfile_name("cred.json", scope)
+    cred_json_str = Config.GOOGLE_CREDENTIALS
+
+    cred_dict = json.loads(cred_json_str)
+    cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(cred_dict, scope)
+    client = gspread.authorize(creds)
+    spreadsheet = client.open_by_url(
+        "https://docs.google.com/spreadsheets/d/17lcrlxUhcervwQTOctLZkdvBVpAwyuWu7DQQ3d_oVSQ/edit?usp=sharing"
+    )
+
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    # creds = ServiceAccountCredentials.from_json_keyfile_name("cred.json", scope)
+    cred_json_str = Config.GOOGLE_CREDENTIALS
+
+    cred_dict = json.loads(cred_json_str)
+    cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(cred_dict, scope)
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_url(
+        "https://docs.google.com/spreadsheets/d/17lcrlxUhcervwQTOctLZkdvBVpAwyuWu7DQQ3d_oVSQ/edit?usp=sharing"
+    ).sheet1
+    phone_column = sheet.col_values(5)
+
+    sheet = spreadsheet.sheet1
+    name = data["name"]
+    surname = data["secondName"]
+    year = data["years"]
+    password = data["password"]
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    number = number.replace("(", "").replace(")", "").replace(" ", "").replace("+", "")
+    data = await state.get_data()
+    num = data.get("num", [])
+    first_name = data.get("first_name", [])
+    last_name = data.get("last_name", [])
+    num = num[0]
+    if number == str(num):
+        phone = number
+        user_phone_map[message.from_user.id] = phone
+        conf = "Confirmed"
+        user_data = [conf, first_name[0], last_name[0], num]
+        for i in range(0, 99):
+            user_data.append(0)
+        sheet.append_row(user_data)
+        user_data.clear()
+        await message.answer(
+            "Номер підтверджено. Вітаємо в клубі розумників та розумниць! 😉"
+        )
+        # await message.answer(
+        #     "Залишилось лише увійти у свій акаунт, для цього натисни кнопку нижче",
+        #     reply_markup=kb.singIn,
+        # )
+        await state.clear()
+    else:
+        await message.answer(
+            "Номер на якому знаходиться телеграм не співпадає з номером вказаним при реєстрації",
+            reply_markup=kb.singIn,
+        )
+        await state.clear()
 
 # Увійти
 @router.message(F.text == "Увійти в акаунт")
