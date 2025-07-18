82bd1eb1459412e43d1f5d5d07af6fcaa1862177
slava-bos-top <105057483+slava-bos-top@users.noreply.github.com>
2025-07-18 20:51:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,18 @@ class Config:
     DEBUG: bool
     FIRST_DATE: str
     GOOGLE_CREDENTIALS: str
+    CLOUDINARY_CLOUD_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
 
     @classmethod
     def load(cls):
         load_dotenv()
 
         cls.GOOGLE_CREDENTIALS = os.getenv("GOOGLE_CREDENTIALS")
+        cls.CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+        cls.CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+        cls.CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
         cls.BOT_TOKEN = os.getenv("BOT_TOKEN")
         if not cls.BOT_TOKEN:
             raise ValueError("❌ BOT_TOKEN is missing!")
@@ -23,3 +29,4 @@ class Config:
         cls.ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
         cls.DEBUG = os.getenv("DEBUG", "False") == "True"
         cls.FIRST_DATE = os.getenv("FIRST_DATE", "")
+
