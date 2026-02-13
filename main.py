3507d6ea63b4dbaae96771831096355c447cc4e8
slava-bos-top <105057483+slava-bos-top@users.noreply.github.com>
2026-02-13 23:12:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram_sqlite_storage.sqlitestore import SQLStorage
 from aiogram import Router
 
-# storage = SQLStorage("fsm_db.db", serializing_method="json")
-from redis.asyncio import Redis
-from aiogram.fsm.storage.redis import RedisStorage
+storage = SQLStorage("fsm_db.db", serializing_method="json")
+# from redis.asyncio import Redis
+# from aiogram.fsm.storage.redis import RedisStorage
 
-redis = Redis.from_url(Config.REDIS_URL)
+# redis = Redis.from_url(Config.REDIS_URL)
 
-storage = RedisStorage(redis=redis)
+# storage = RedisStorage(redis=redis)
 bot = Bot(token=Config.BOT_TOKEN)
 dp = Dispatcher(storage=storage)
 
