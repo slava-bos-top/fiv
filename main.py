4a6d5e5af357bdd4f144315ce78137e7010ecb9a
slava-bos-top <105057483+slava-bos-top@users.noreply.github.com>
2026-02-13 22:08:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from config import Config
 from aiogram_sqlite_storage.sqlitestore import SQLStorage
 from aiogram import Router
 
-Config.load()
-storage = SQLStorage("fsm_db.db", serializing_method="json")
+# storage = SQLStorage("fsm_db.db", serializing_method="json")
+from redis.asyncio import Redis
+from aiogram.fsm.storage.redis import RedisStorage
+
+redis = Redis.from_url(Config.REDIS_URL)
+
+storage = RedisStorage(redis=redis)
 bot = Bot(token=Config.BOT_TOKEN)
 dp = Dispatcher(storage=storage)
 
@@ -21,6 +26,7 @@ async def main():
 
 if __name__ == "__main__":
     try:
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type: ignore
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Бот вимкнено!")
